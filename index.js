c8b052bae4c88b9c41f218a8a8ea3bf921128b89
hbounouidar <bounouidarhamza@gmail.com>
2020-04-20 21:42:54 +0200

    deploying node

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const io = socketio(server);
 
 
 
-// app.use((req,res,next) => {
-//         res.setHeader('Access-Control-Allow-Origin', '*');
-//         res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE', 'PATCH')
-//         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-//         next();
-//     })
+app.use((req,res,next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+    //     res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE', 'PATCH')
+    //     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        next();
+    })
 app.use(router);
 app.use(cors());
 
