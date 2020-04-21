9c412fc37a2803fed9a692b15f300a25c31f77e7
hbounouidar <bounouidarhamza@gmail.com>
2020-04-21 14:23:54 +0200

    deploy heroku

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const io = socketio(server);
 
 
 
-// app.use((req,res,next) => {
-//         res.setHeader('Access-Control-Allow-Origin', '*');
-//         res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE', 'PATCH')
-//         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-//         
-//     next();
-//     })
-app.use(cors());
+app.use((req,res,next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE', 'PATCH')
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        
+    next();
+    })
+// app.use(cors());
 app.use(router);
 
 
@@ -60,4 +60,4 @@ io.on('connect', (socket) => {
   })
 });
 
-server.listen(process.env.PORT || 1337, () => console.log(`Server has started.`));
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
